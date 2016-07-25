b3c80854e5854abb4b1f7ce0e122cac3ae52b6bb
Keerthini K <keerthini.developer@gmail.com>
2016-07-25 19:31:18 +0530

    animation added

--- app/src/main/java/com/books/earthmedia/earthbeat/EarthBeatFlashCard.java
+++ app/src/main/java/com/books/earthmedia/earthbeat/EarthBeatFlashCard.java
@@ -7,6 +7,9 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,6 +21,7 @@ import android.widget.Toast;
 public class EarthBeatFlashCard extends Activity implements SimpleGestureFilter.SimpleGestureListener {
     private SimpleGestureFilter detector;
     private TextView questionTextView, answerTextView;
+    LinearLayout answerLayout;
     private int i = 0;
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +29,7 @@ public class EarthBeatFlashCard extends Activity implements SimpleGestureFilter.
         setContentView(R.layout.fragment_flashcard);
         questionTextView = (TextView) findViewById(R.id.questions);
         answerTextView = (TextView) findViewById(R.id.answer);
-
+        answerLayout = (LinearLayout) findViewById(R.id.answerLayout);
         questionTextView.setText(DataArray.questions[i]);
         // Detect touched area
         detector = new SimpleGestureFilter(this, this);
@@ -52,8 +56,11 @@ public class EarthBeatFlashCard extends Activity implements SimpleGestureFilter.
             case SimpleGestureFilter.SWIPE_LEFT:
                 str = "Swipe Left";
                 if (i < DataArray.questions.length - 1) {
+
+
                     i++;
                     questionTextView.setText(DataArray.questions[i]);
+
                     answerTextView.setVisibility(View.INVISIBLE);
                 } else {
                     Toast.makeText(getApplicationContext(), "qUESTIONS Over", Toast.LENGTH_SHORT).show();
@@ -64,6 +71,9 @@ public class EarthBeatFlashCard extends Activity implements SimpleGestureFilter.
                 break;
             case SimpleGestureFilter.SWIPE_UP:
                 str = "Swipe Up";
+                final Animation slideup = AnimationUtils.loadAnimation(this,R.anim.slide_up);
+                //  final Animation trainfade = AnimationUtils.loadAnimation(this,R.anim.slide_down);
+                answerTextView.startAnimation(slideup);
                 answerTextView.setText(DataArray.answer[i]);
                 answerTextView.setVisibility(View.VISIBLE);
                 break;
